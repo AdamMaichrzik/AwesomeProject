aa00536df8c88e527d68504b521526bef871d798
Adam <adam.maichrzik@wp.pl>
2023-12-02 15:15:30 +0100

    Background color to blue

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: 'white',
+    backgroundColor: 'blue',
   },
   scoreBoardView: {
     flex: 1,
