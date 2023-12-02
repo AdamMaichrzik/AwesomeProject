93fc5858b6655496d30af20a902ae028f3406f44
Adam <adam.maichrzik@wp.pl>
2023-12-02 14:55:33 +0100

    Backgound color to white

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
   },
   scoreBoardView: {
     flex: 1,
